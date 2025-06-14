6fd9f7b5481976646d8a50f85fa3a07ef6e94fb9
name <suradaramesh4@gmail.com>
2025-06-14 18:41:22 +0530

    setting up client side inital commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-black bg-slate-400"> hello world </p>
+    </div>
+  );
+};
+
+export default App;
